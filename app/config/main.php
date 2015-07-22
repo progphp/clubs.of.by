9311fb5729be4280a98a2312daeefa73375f7978
Михаил Корольчук <korolchuk.mikhail@gmail.com>
2015-07-23 01:19:05 +0200

    Initial Project DB

--- app/config/main.php
+++ app/config/main.php
@@ -6,46 +6,44 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 $config = array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Yii Blog Demo',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Yii Blog Demo',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+    'preload' => array('log'),
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-	),
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
 
-	'defaultController'=>'post',
+    'defaultController' => 'post',
 
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'post/<id:\d+>/<title:.*?>'=>'post/view',
-				'posts/<tag:.*?>'=>'post/index',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-	),
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'rules' => array(
+                'post/<id:\d+>/<title:.*?>' => 'post/view',
+                'posts/<tag:.*?>' => 'post/index',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ),
+    ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>require(dirname(__FILE__).'/params.php'),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => require(dirname(__FILE__) . '/params.php'),
 );
 
-switch($_SERVER['HOST']){
+switch ($_SERVER['HOST']) {
     case 'localhost':
         $config_host = include_once("local.php");
         break;
